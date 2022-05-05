2e0ce7e1ccec8356f30adb0e2dcc3e3d431efa1c
Hélio Lopes <57041899+helio-lopes13@users.noreply.github.com>
2022-05-05 11:07:37 -0300

    Código da fechadura eletrônica concluído

--- fechadura.c
+++ fechadura.c
@@ -7,13 +7,13 @@
 #use rs232(baud=9600, xmit = PIN_C6, rcv = PIN_C7)
 #include "12Keys.c"
 
-boolean flag;
-int i;
+int i, tamanho_senha = 5;
 
 void main() {
 	char senha[5] = {'5', '0', '2', '0', '6'};
 	char senha_digitada[5];
 	int contador = 0;
+	boolean comparador;
 	tecla_pres = 0;
 
 	port_b_pullups(True);
@@ -22,33 +22,56 @@ void main() {
 		tecla = trata_teclas();
 
 		if (tecla_pres) {
-			printf("%c", tecla);
+			printf("%c  ", tecla);
 			
-			senha_digitada[contador] = tecla;
-			contador++;
-			
-			if (contador == 5) {
-				for (i = 0; i < 5; i++) {
-					printf("%c", senha_digitada[i]);            
+			if (tecla == '#') {
+				for (i = 0; i < tamanho_senha; i++) {
+					printf("%c", senha_digitada[i]);
 				}
-
+				
+				printf("\n");
+				
+				comparador = true;
+				for (i = 0; i < tamanho_senha; i++) {
+					comparador = comparador && (senha[i] == senha_digitada[i]);
+					
+					
+					if (!comparador) {
+						printf("Senha errada\n");
+						output_high(vermelho);
+					}
+				}
+				
+				if (comparador) {
+					printf("Senha correta\n");
+					output_low(vermelho);
+					output_high(verde);
+					delay_ms(5000);
+					output_low(verde);
+				}
+				
 				contador = 0;
+				for (i = 0; i < tamanho_senha; i++) {
+					senha_digitada[i] = '\0';     
+				}
+			} else {
+				if (contador == tamanho_senha) {
+					contador = 0;
+					for (i = 0; i < tamanho_senha; i++) {
+						senha_digitada[i] = '\0';
+					}
+				}
+				
+				senha_digitada[contador] = tecla;
+				
+				contador++;
+				
+				for (i = 0; i < tamanho_senha; i++) {
+					printf("%c", senha_digitada[i]);
+				}
+				
+				printf("\n");
 			}
 		}
-		
-		switch(tecla) {
-			case '1': output_high(verde);break;
-			case '2': output_high(amarelo);break;
-			case '3': output_high(vermelho);break;
-			case '4': output_low(verde);break;
- 			case '5': output_low(amarelo);break;
-			case '6': output_low(vermelho);break;
-			case '7': output_high(verde);output_high(amarelo);break;
-			case '8': output_high(verde); output_high(vermelho);break;
-			case '9': output_high(amarelo);output_high(vermelho);break;
-			case '0': output_high(amarelo);output_high(verde);output_high(vermelho);break;
-			case '*': output_low(amarelo);output_low(verde);output_low(vermelho);break;
-			case '#': output_low(amarelo);output_low(verde);output_low(vermelho);break;
-      }
-   }
+	}
 }
